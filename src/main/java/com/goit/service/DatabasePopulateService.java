48895614ac02c8e94a5da80066dae1c7c06030bd
A1MaHaiD <super.lukianchuk2010@gmail.com>
2023-05-07 17:34:03 +0300

    perhaps it should be so

--- src/main/java/com/goit/service/DatabasePopulateService.java
+++ src/main/java/com/goit/service/DatabasePopulateService.java
@@ -6,6 +6,7 @@ import com.goit.reader.Reader;
 import com.goit.service.datasource.DatabasePopulateDatasource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class DatabasePopulateService {
@@ -14,16 +15,20 @@ public class DatabasePopulateService {
     private static final String PASSWORD = "12345";
 
     public static void main(String[] args) {
-        DatabasePopulateDatasource data = DatabasePopulateDatasource.of(MYH2_URL, USERNAME, PASSWORD);
         Reader sb = new Reader("sql/populate_db.sql");
-        Connection conn = Database.getInstance().getConnection();
+        DatabasePopulateDatasource data = DatabasePopulateDatasource.of(MYH2_URL, USERNAME, PASSWORD);
+//        Connection conn = Database.getInstance().getConnection();
         try {
-            Statement stmt = conn.createStatement();
+            /*Statement stmt = conn.prepareStatement(sb.read());
             stmt.execute(sb.read());
             stmt.close();
-            conn.close();
+            conn.close()*/;
+            PreparedStatement preparedStatement = data.prepareStatement(sb.read());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            preparedStatement.close();
         } catch (Exception e) {
-            throw new DbException("Connection or statement failed",e);
+            throw new DbException("Connection or statement failed", e);
         }
     }
 }
